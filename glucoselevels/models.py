c4e63134a0ab37cd09bedeb02127b1911867223c
Ole Nascimento <olenascimento@gmail.com>
2021-11-16 12:22:44 +0100

    github commit

--- glucoselevels/models.py
+++ glucoselevels/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Values(models.Model):
-    user_id = models.CharField(unique=True)
+    user_id = models.CharField(max_length=50, unique=True)
     time = models.DateTimeField(default=None)
     value = models.PositiveBigIntegerField(default=None)
     
