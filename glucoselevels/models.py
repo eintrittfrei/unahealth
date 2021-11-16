2a8b40b4af3ea7e4e965469dc8c8005a3ac64473
Ole Nascimento <olenascimento@gmail.com>
2021-11-16 11:24:11 +0100

    Values model created

--- glucoselevels/models.py
+++ glucoselevels/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Values(models.Model):
+    user_id = models.CharField(unique=True)
+    time = models.DateTimeField(default=None)
+    value = models.PositiveBigIntegerField(default=None)
+    
+    
