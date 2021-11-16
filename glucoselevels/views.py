9e1a6b66942bbc6486a8a81e07365c84e8f465d8
Ole Nascimento <olenascimento@gmail.com>
2021-11-16 13:00:01 +0100

    build view all

--- glucoselevels/views.py
+++ glucoselevels/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from .models import Values
+
+from serializers import ValuesSerialzier
 
 # Create your views here.
+
+class ValuesListView(APIView):
+  
+    def get(self, _request):
+        values = Values.objects.all() 
+        serialized_values = ValuesSerialzier(values, many=True)
+        return Response(serialized_values.data, status=status.HTTP_200_OK)
+
+    
